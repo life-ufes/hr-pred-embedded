16db4627f86bbd0df802c6d285815ac558e9f0d3
vortex2jm <joaopaulomoura5328@gmail.com>
2026-02-25 23:57:05 -0300

    chore: update task stack size

--- main/main.c
+++ main/main.c
@@ -9,10 +9,10 @@ void app_main(void)
     init_pipeline();
 
     xTaskCreate(task_rx, "task_rx", 4096, NULL, 5, NULL);
-    xTaskCreate(task_preprocess, "preprocess_task", 2048, NULL, 5, NULL);
+    xTaskCreate(task_preprocess, "preprocess_task", 1024, NULL, 5, NULL);
 
 #ifdef CONFIG_EXPONENTIAL_APPROXIMATION_MODEL
-    xTaskCreate(task_inference_eam, "inference_task", 4096, NULL, 5, NULL);
+    xTaskCreate(task_inference_eam, "inference_task", 1024, NULL, 5, NULL);
 #else
     xTaskCreate(task_inference_dem, "inference_task", 4096, NULL, 5, NULL);
 #endif
