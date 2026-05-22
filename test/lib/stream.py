f17c0d6122c4ffb68a30dce9efdbefa6340f20dc
Higor David O. <43549329+Rigor-do@users.noreply.github.com>
2026-05-22 15:40:27 -0300

    Amend to last pushed commit

--- test/lib/stream.py
+++ test/lib/stream.py
@@ -27,7 +27,7 @@ class DataStreamer:
 
 
     def _load_and_prepare(self):        
-        desired_cols = ["acc_x", "acc_y", "acc_z", "timestamp", "hr", "timestamp_hr", "train"]
+        desired_cols = ["acc_x", "acc_y", "acc_z", "gyro_x", "gyro_y", "gyro_z", "timestamp", "hr", "timestamp_hr", "train"]
 
         df = pd.read_csv(self.csv_path, usecols=desired_cols)
 
